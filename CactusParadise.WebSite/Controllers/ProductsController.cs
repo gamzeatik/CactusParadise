e5dfd0d9b69b94fb48190d939ee81f0c112849da
gamzeatik <82980529+gamzeatik@users.noreply.github.com>
2022-01-18 21:35:09 +0300

    Initial commit

---
+++ CactusParadise.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using CactusParadise.WebSite.Models;
+using CactusParadise.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CactusParadise.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+        [HttpGet]
+        [Route("Rate")]
+        public ActionResult Get([FromQuery] string ProductId, [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+                return Ok();
+        }
+    }
+}
